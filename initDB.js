2b037212b20444f9277d46681b01de9c9d3be8eb
aawadall <aawadall@ualberta.ca>
2019-02-23 10:15:17 -0500

    updated network ID

--- initDB.js
+++ initDB.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 const user='root';
 const password='secret';
-const host = '127.0.0.1';
+const host = '172.18.0.3';
 const port = 27017
 const url= `mongodb://${user}:${password}@${host}:${port}`;
 
