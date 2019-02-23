bf260340ecb1eb15dc92cc78ab6a1bab13469b17
aawadall <aawadall@ualberta.ca>
2019-02-23 09:20:00 -0500

    could not create a database yet

--- initDB.js
+++ initDB.js
@@ -1,15 +1,25 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const url= 'mongodb://localhost:27017';
+
+const user='root';
+const password='secret';
+const host = '127.0.0.1';
+const port = 27017
+const url= `mongodb://${user}:${password}@${host}:${port}`;
 
 const client = new MongoClient(url);
-
+const vaultDatabase = 'vault';
 // Use connect method to connect to the Server
-client.connect(function(err) {
+
+client.connect(function(err, db) {
     assert.equal(null, err);
-    console.log("Connected successfully to server");
-  
-    //const db = client.db(dbName);
-  
+
+    console.log(`Connected to server `);
+    console.log(db);
+
+    client.db(vaultDatabase);
+
     client.close();
   });
+
+  
