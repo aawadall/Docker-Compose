d47c06580185e7439335bb0b1065e64d069808e2
aawadall <aawadall@ualberta.ca>
2019-02-23 08:42:15 -0500

    tested connection to mongodb and added git ignore

---
+++ initDB.js
@@ -0,0 +1,15 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url= 'mongodb://localhost:27017';
+
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+  
+    //const db = client.db(dbName);
+  
+    client.close();
+  });
